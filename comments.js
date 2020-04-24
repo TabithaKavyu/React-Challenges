300a1c33883444f928b7ab478bfd3b766bbfb604
Thabitha Kavyu <tabithakavyu4@gmail.com>
2020-04-25 02:13:29 +0300

    Create commets.js

---
+++ comments.js
@@ -0,0 +1,11 @@
+/*SX is a syntax that gets compiled into valid JavaScript. Sometimes, for readability, you might need to add comments to your code.
+Like most programming languages, JSX has its own way to do this.To put comments inside JSX, you use the syntax {/* */} to wrap around the comment text*/
+//Example
+
+const JSX = (
+  <div>
+    <h1>This is a block of JSX</h1>
+    <p>Here's a subtitle</p>
+    {/*This does not change anything */}
+  </div>
+);
