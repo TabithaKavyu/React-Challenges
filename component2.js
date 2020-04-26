99ec45fd904f2c774ac7cc31970432e45efe73e9
Thabitha Kavyu <tabithakavyu4@gmail.com>
2020-04-27 01:20:20 +0300

    create component2.js

---
+++ component2.js
@@ -0,0 +1,18 @@
+/*The other way to define a React component is with the ES6 class syntax. In the following example, MyComponent extends React.Component*/
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+  
+   return (
+      <div>
+      <h1>Hello React!</h1>
+      </div>
+    );
+
+
+
+  }
+};
