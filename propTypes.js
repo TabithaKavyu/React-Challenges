68a89a6614a8d627d34e20f863d0825ea81ad241
Thabitha Kavyu <tabithakavyu4@gmail.com>
2020-04-28 00:40:44 +0300

    Create propTypes.js

---
+++ propTypes.js
@@ -0,0 +1,21 @@
+/*React provides useful type-checking features to verify that components receive props of the correct type
+Use PropTypes to Define the Props You Expect*/
+
+const Items = (props) => {
+  return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+};
+
+Items.propTypes = { quantity: PropTypes.number.isRequired }
+
+Items.defaultProps = {
+  quantity: 0
+};
+
+class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <Items />
+  }
+};
