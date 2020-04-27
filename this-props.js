eeff62466e28adcaa3888ec2fe7afe7e7ee5df73
Thabitha Kavyu <tabithakavyu4@gmail.com>
2020-04-28 01:03:23 +0300

    create this-props.js

---
+++ this-props.js
@@ -0,0 +1,38 @@
+/* Access Props Using this.props.
+Anytime you refer to a class component within itself, you use the this keyword. To access props within a class component, you preface the code that you use to access it with this. 
+For example, if an ES6 class component has a prop called data, you write {this.props.data} in JSX.*/
+
+
+class ReturnTempPassword extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+        <div>
+            <p>Your temporary password is: <strong>{this.props.tempPassword}</strong></p>
+           
+        </div>
+    );
+  }
+};
+
+class ResetPassword extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+        <div>
+          <h2>Reset Password</h2>
+          <h3>We've generated a new temporary password for you.</h3>
+          <h3>Please reset this password from your account settings ASAP.</h3>
+       
+      <ReturnTempPassword tempPassword={"forallthingsnottic"} />
+         
+        </div>
+    );
+  }
+};
