c047f66cdd46f5df259352ef2ed3e18cce1f1e4a
Thabitha Kavyu <tabithakavyu4@gmail.com>
2020-04-28 00:01:44 +0300

    create arrayProps.js

---
+++ arrayProps.js
@@ -0,0 +1,22 @@
+/*To pass an array to a JSX element, it must be treated as JavaScript and wrapped in curly braces.*/
+
+const List = (props) => {
+  return <p>{props.tasks.join(', ')}</p>;
+};
+
+class ToDo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>To Do Lists</h1>
+        <h2>Today</h2>
+        <List tasks={["walk dog", "workout"]} />
+        <h2>Tomorrow</h2>
+        <List  tasks={["walk dog", "workout", "jogging"]} />
+      </div>
+    );
+  }
+};
